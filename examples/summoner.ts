3bdc9a7443bfc2d3ed84f693c1e449f5978bab6a
Freddiiy <frederikgaller@live.dk>
2022-08-19 20:51:37 +0200

    ooops

--- examples/summoner.ts
+++ examples/summoner.ts
@@ -1,6 +1,6 @@
 import Reksai from "../src";
 
-const reksai = new Reksai("RGAPI-f14e2073-cdeb-4fb3-8317-1d0158909c3e");
+const reksai = new Reksai("your-api-key");
 const main = async() => {
 	const summoner = await reksai.summoner.bySummonerName("Thiccikyu"); //Get typesafe// summoner object back
 	const summonerName = summoner.name;
