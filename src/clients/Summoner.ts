682ec278405fb337c8468140a9c46b6932618d8a
Freddiiy <frederikgaller@live.dk>
2022-08-19 17:43:08 +0200

    add regional support for summoner endpoint

--- src/clients/Summoner.ts
+++ src/clients/Summoner.ts
@@ -8,14 +8,16 @@ class Summoner {
 
 	constructor(apiKey: string) {
 		this.apiKey = apiKey;
-		this.endpoint = "/lol/summoner/v4/summoners"
+		this.endpoint = "lol/summoner/v4/summoners"
 	}
 
+	//TODO: Add regional support asap
 	async byAccount(encrypyedAccountId: string): Promise<ISummoner> {
 		return await typeFetch<ISummoner>(`${apiUrl.EUW}/${this.endpoint}/by-account/${encrypyedAccountId}`,this.apiKey);
 	}
 
 	async bySummonerName(summonerName: string): Promise<ISummoner> {
+		console.log(`${apiUrl.EUW}/${this.endpoint}/by-name/${summonerName}`);
 		return await typeFetch<ISummoner>(`${apiUrl.EUW}/${this.endpoint}/by-name/${summonerName}`,this.apiKey);
 	}
 
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,10 @@
 import Reksai from "./reksai";
+
+const reksai = new Reksai("RGAPI-5ae17bd4-8c18-4d73-9baa-b4f0a38a58a9")
+
+async function test() {
+	const summoner = await reksai.summoner.bySummonerName("MAMMOTHMAN65");
+	console.log(summoner);
+}
+test();
 export default Reksai
